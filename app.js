3ae41d71221268c50f98388170a4c230cc6c25e1
Wassword <ahmedsaba271@gmail.com>
2024-10-16 09:56:50 -0400

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Pizzas are better with pineaple!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
